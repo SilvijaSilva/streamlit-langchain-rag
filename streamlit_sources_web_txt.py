f76ebe21180b5863d1bd4b4985f1bf96bfe499fd
SilvijaSilva <silvija.klejevskyte@gmail.com>
2025-07-02 18:22:04 +0300

    comments deleted

--- streamlit_sources_web_txt.py
+++ streamlit_sources_web_txt.py
@@ -12,56 +12,46 @@ from langchain import hub
 from dotenv import load_dotenv
 from langchain_openai.chat_models import ChatOpenAI
 from langchain_community.document_loaders import WebBaseLoader
-import bs4  # BeautifulSoup for parsing HTML
+import bs4
 
-# Load environment variables from .env file
 load_dotenv()
 
 token = os.getenv("MY_TOKEN")
 endpoint = "https://models.github.ai/inference"
 model = "openai/gpt-4.1-nano"
 
-# Load documents from web sources
 web_loader = WebBaseLoader(
     web_paths=[
         "https://lt.wikipedia.org/wiki/Širvintos",
-        #"https://www.vle.lt/straipsnis/sirvintos/"
         "https://www.yesforskills.lt/ka-verta-pamatyti-sirvintose-lankytinos-vietos-su-vaikais-ir-idomus-faktai-apie-si-miesta"
     ]
 )
 web_docs = web_loader.load()
 
-# Load documents from local text file
-file_loader = TextLoader("sirvintos_info.txt", encoding="utf-8") #character encoding
+file_loader = TextLoader("sirvintos_info.txt", encoding="utf-8")
 local_docs = file_loader.load()
 
-# Combine documents from both sources
 all_docs = web_docs + local_docs
 
-# Split documents into chunks
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=10)
 splits = text_splitter.split_documents(all_docs)
 
-# Create vector store
 embeddings=OpenAIEmbeddings(
     model="text-embedding-3-small",
     base_url="https://models.inference.ai.azure.com",
-    api_key=token, # type: ignore
+    api_key=token,
 )
 
 vectorstore = InMemoryVectorStore(embeddings)
 _ = vectorstore.add_documents(documents=splits)
 
-# Retriever setup
 retriever = vectorstore.as_retriever(search_kwargs={"k": 3})
 prompt = hub.pull("rlm/rag-prompt")
 
-# Format documents for output
 def format_docs(docs):
     #print(docs)
     return "\n\n".join(doc.page_content for doc in docs)
 
-# Streamlit app setup
 st.title("Klausk ir sužinok viską apie Širvintas")
 
 def generate_response(input_text):
@@ -84,7 +74,6 @@ def generate_response(input_text):
         with st.expander(f"Šaltinis {i}"):
             st.write(f"**Turinys:** {doc.page_content}")
 
-# UI for user input
 with st.form("my_form"):
     text = st.text_area(
         "Įvesk klausimą:",
